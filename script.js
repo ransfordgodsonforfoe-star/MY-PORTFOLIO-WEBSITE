f9b63ef0863abe8c30d6ca6cf63c0900bc745bda
Ransford Godson <your-email@example.com>
2026-06-13 12:47:09 -0700

    Updated Website

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-// HAMBURGER MENU
+
+// MENU
 const toggle = document.getElementById("menu-toggle");
 const nav = document.getElementById("nav-links");
 
-toggle.addEventListener("click", () => {
+toggle.addEventListener("click", ()=>{
 nav.classList.toggle("active");
 });
 
@@ -14,8 +15,29 @@ function type(){
 if(i < text.length){
 document.getElementById("typing").innerHTML += text.charAt(i);
 i++;
-setTimeout(type, 80);
+setTimeout(type, 70);
 }
 }
 
 type();
+
+// PARTICLES
+const container = document.getElementById("particles");
+
+for(let i=0;i<40;i++){
+let dot = document.createElement("div");
+
+dot.style.position="absolute";
+dot.style.width="4px";
+dot.style.height="4px";
+dot.style.background="#38bdf8";
+dot.style.borderRadius="50%";
+dot.style.opacity=Math.random();
+
+dot.style.left=Math.random()*100+"vw";
+dot.style.top=Math.random()*100+"vh";
+
+dot.style.animation="float "+(3+Math.random()*5)+"s infinite";
+
+container.appendChild(dot);
+}
